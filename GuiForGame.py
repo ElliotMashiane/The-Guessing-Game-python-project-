d42dcd57d1aa3a123d286693be93b6db1b33f030
Elliot Mashiane <elliot.mashiane@gmail.com>
2024-12-29 11:31:25 +0200

    Add display update functionality for selected number and result

--- GuiForGame.py
+++ GuiForGame.py
@@ -1,5 +1,12 @@
 from tkinter import *
 
+# Function to handle button press
+
+
+def update_display(number):
+    number_disp_label.config(text=f"Selected: {number}")
+
+
 window = Tk()
 
 window.geometry("420x420")
@@ -30,7 +37,6 @@ time_display_lbl.place(x=50, y=0)
 
 
 def countdown(seconds=30):
-
     if seconds >= 0:
         # Update the label with the current time
         time_display_lbl.config(text=f"{seconds:02d} seconds")
@@ -46,13 +52,11 @@ start_button = Button(window, text='Start Game', command=lambda: countdown(30))
 start_button.pack()
 
 # Label For welcoming note
-
 welcome_label = Label(
     window, text='Welcome to the Guess the number Game!')
 welcome_label.place(x=100, y=80)
 
 # Label For Game Instructions
-
 Instrctions_label = Label(
     window, text='Choose a number between 1 and 10')
 Instrctions_label.place(x=115, y=100)
@@ -78,8 +82,17 @@ for row_index, row in enumerate(button_labels):
         x = start_x + col_index * (button_width + button_spacing)
         y = start_y + row_index * (button_height + button_spacing)
         button = Button(window, text=str(label), font=(
-            'Arial', 12), width=5, height=2)
+            'Arial', 12), width=5, height=2, command=lambda num=label: update_display(num))
         button.place(x=x, y=y)
 
+# Create a label to display the number input
+number_disp_label = Label(window, text="Selected: None",
+                          font=('Arial', 18), bg='#d5e2e6')
+number_disp_label.place(x=220, y=220)
+
+# Create a label to display the result
+result_label = Label(window, text="Result: None",
+                     font=('Arial', 18), bg='#d5e2e6')
+result_label.place(x=220, y=300)
 
 window.mainloop()
